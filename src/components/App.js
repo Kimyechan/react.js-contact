2213b29245d9988ef84fdef3d1b1a303feb90ebb
Kimyechan <vlvkcjswo7@naver.com>
2020-02-14 19:35:58 +0900

    Contact 검색기능 구현|input, sort, filter

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Contact from './Contact';
 
 class App extends React.Component {
 
@@ -12,11 +13,7 @@ class App extends React.Component {
     render(){
 
         return (
-            <div>
-                <h1>Hello React Skeleton!!!!!!</h1>
-                <h2>{this.state.value}</h2>
-                <button onClick={() => { this.setState({value: this.state.value + 1}) }}>CLICK ME</button>
-            </div>
+            <Contact/>
         );
     }
 }
